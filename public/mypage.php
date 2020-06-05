053167c778365bf4a486d212a5caaa9b2ee6b63b
Alex Duong <alex.duong@live.se>
2020-06-05 10:20:13 +0200

    added loginpage, mypage, registerpage

---
+++ public/mypage.php
@@ -0,0 +1,23 @@
+<?php
+    $pageTitle = "Mypage";
+    $pageId = "me";
+?>
+<?php include('layout/header.php'); ?>
+
+    <!-- Sidans/Dokumentets huvudsakliga innehåll -->
+    <div id="content">
+        <article class="justify border" style="width:80%">
+            <h1>Om mig själv</h1>
+
+            <figure class="right top">
+            <figcaption>
+            textblablabla
+            </figcaption>
+            </figure>
+
+        <hr>
+        </article>
+    </div>
+
+<?php include('layout/footer.php'); ?>
+
